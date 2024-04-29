be47a4e509cd209c5f1012a96e533d920c784ab3
D4void <d4void@m4he.fr>
2024-04-30 00:28:44 +0200

    update

--- psql-backup.sh
+++ psql-backup.sh
@@ -2,5 +2,12 @@
 
 source .env
 
+backupfile="${POSTGRES_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump.gz"
 #docker exec -it MyJoplinPostgres pg_dump -U ${POSTGRES_USER} ${POSTGRES_DATABASE} | gzip -9 > ./${POSTGRES_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").sql.gz
-docker exec -it MyJoplinPostgres pg_dump -F t -U ${POSTGRES_USER} ${POSTGRES_DATABASE} | gzip -9 > ./${POSTGRES_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump.gz
+docker exec -it MyJoplinPostgres pg_dump --format=custom --compress=9 -U ${POSTGRES_USER} ${POSTGRES_DATABASE} -f /${backupfile}
+docker cp MyJoplinPostgres:/${backupfile} .
+
+#docker_command="PGPASSWORD=${POSTGRES_PASSWORD} pg_dump --format=custom --compress=9 -h MyJoplinPostgres \
+#-U ${POSTGRES_USER} ${POSTGRES_DATABASE} \
+#-f /backup/${POSTGRES_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump.gz"
+#docker run --rm -it --network MyJoplinNet -v .:/backup postgres:16.2-alpine3.19 -c "$docker_command"
--- psql-restore.sh
+++ psql-restore.sh
@@ -2,10 +2,11 @@
 
 source .env
 
-gunzip $1
+#gunzip $1
 
 #docker cp $(dirname $1)/$(basename $1 .gz) MyJoplinPostgres:/db.sql
-docker cp $(dirname $1)/$(basename $1 .gz) MyJoplinPostgres:/db.dump
+#docker cp $(dirname $1)/$(basename $1 .gz) MyJoplinPostgres:/db.dump
+docker cp $1 MyJoplinPostgres:/db.dump.gz
 
 #docker exec -it MyJoplinPostgres cat /db.sql |psql -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE}
-docker exec -it MyJoplinPostgres pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE} --no-owner --single-transaction /db.dump
+docker exec -it MyJoplinPostgres pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE} --no-owner --single-transaction /db.dump.gz
