fb71205d32cbf2a10fda34518a99002b57dd894d
D4void <d4void@m4he.fr>
2024-04-29 22:48:16 +0200

    update

--- psql-restore.sh
+++ psql-restore.sh
@@ -2,8 +2,8 @@
 
 source .env
 
-gunzip %1
+gunzip $1
 
-docker cp $(basename %1 .gz) MyJoplinPostgres:/db.sql
+docker cp $(basename $1 .gz) MyJoplinPostgres:/db.sql
 
 docker exec -it MyJoplinPostgres pg_restore ${POSTGRES_USER} -d ${POSTGRES_DATABASE} --no-owner -1 /db.sql
