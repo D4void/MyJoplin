7ec75458266f6f035eda91a886a65f1ae26f030c
D4void <d4void@m4he.fr>
2024-05-04 01:08:53 +0200

    update

--- psql-backup.sh
+++ psql-backup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-source .env
+source $0/.env
 
-backupfile="${POSTGRES_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump.gz"
+backupfile="${POSTGRES_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump"
 #docker exec -it MyJoplinPostgres pg_dump -U ${POSTGRES_USER} ${POSTGRES_DATABASE} | gzip -9 > ./${POSTGRES_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").sql.gz
-docker exec -it MyJoplinPostgres pg_dump --format=custom --compress=9 -U ${POSTGRES_USER} ${POSTGRES_DATABASE} -f /${backupfile}
+
+echo "Backuping Postgres ${POSTGRES_DATABASE} database"
+
+docker exec -it MyJoplinPostgres pg_dump --format=custom --compress=6 -U ${POSTGRES_USER} ${POSTGRES_DATABASE} -f /${backupfile}
+
+echo "Copy backup from container"
+
 docker cp MyJoplinPostgres:/${backupfile} .
+
+echo "End."
 
 #docker_command="PGPASSWORD=${POSTGRES_PASSWORD} pg_dump --format=custom --compress=9 -h MyJoplinPostgres \
 #-U ${POSTGRES_USER} ${POSTGRES_DATABASE} \
--- psql-restore.sh
+++ psql-restore.sh
@@ -6,7 +6,12 @@ source .env
 
 #docker cp $(dirname $1)/$(basename $1 .gz) MyJoplinPostgres:/db.sql
 #docker cp $(dirname $1)/$(basename $1 .gz) MyJoplinPostgres:/db.dump
+
+echo "Copying backup $1 into container"
 docker cp $1 MyJoplinPostgres:/db.dump.gz
 
+echo "Restoring backup into Postgres"
 #docker exec -it MyJoplinPostgres cat /db.sql |psql -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE}
 docker exec -it MyJoplinPostgres pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE} --no-owner --single-transaction /db.dump.gz
+
+echo "End."
