a3abb39e31422534523be9b9cad9a1e989c533c2
D4void <d4void@m4he.fr>
2024-05-07 18:32:48 +0200

    update

--- psql-restore.sh
+++ psql-restore.sh
@@ -5,8 +5,13 @@
 source $(dirname $0)/.env
 
 if [[ -f "$1" ]]; then
-    echo "Restoring backup $1 into Postgres"
-    docker exec MyJoplinPostgres pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE} --no-owner --single-transaction /backup/$1
+    read -p "Are you sure you want to restore ${POSTGRES_DATABASE} with dump $1 ? " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Restoring backup $1 into Postgres"
+        docker exec MyJoplinPostgres pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE} --no-owner --single-transaction /backup/$1
+    fi
 else
     echo "$1 doesn't exist!"
 fi
